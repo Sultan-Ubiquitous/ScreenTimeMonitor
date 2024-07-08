af558a1077642d9452877dc1909c9f38f41c6d23
Azim <azimakhorajiya@gmail.com>
2024-07-09 00:15:20 +0300

    added proper formating for time

--- scripFinale.py
+++ scripFinale.py
@@ -1,8 +1,24 @@
 import subprocess
 import time 
 
-prev_window = ""
-start_time = time.perf_counter()
+def format_time(duration):
+
+  if duration < 60:
+    unit = "seconds"
+  elif duration < 3600:
+    duration /= 60  # Convert to minutes
+    unit = "minutes"
+  else:
+    duration /= 3600  # Convert to hours
+    unit = "hours"
+
+  formatted_time = f"{duration:.2f}"
+
+  return f"{formatted_time} {unit}"
+
+
+prev_window = None
+start_time = time.time()
 
 # cmd is the bash command
 # In output variable we run the command and capture the output
@@ -11,9 +27,17 @@ while True:
     cmd = 'xprop -id $(xprop -root _NET_ACTIVE_WINDOW | cut -d " " -f 5) WM_NAME | cut -d \'"\' -f 2'
     output = subprocess.check_output(cmd, shell=True)
     current_window = output.decode('utf-8').strip()
-
-    print(current_window)
-    subprocess.call(["sleep", "3"])
-    # Your code here
+    
+    if current_window != prev_window:
+        end_time = time.time()
+        duration = format_time(end_time - start_time)
+        
+        if prev_window is not None:
+            print(f"Window: {prev_window}, Duration: {duration}")
+        prev_window = current_window
+        start_time = end_time
+        
+    subprocess.call(["sleep", "1"])
+    
 
     
